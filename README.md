78dbef1f7c9a435b70e92028d4e96a193c2e6d45
Aaron Zauner <azet@azet.org>
2014-07-26 04:27:37 +0200

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+## dietfefe
+blog.fefe.de - refined
+
+- install python
+- `pip install scrapy`
+- `scrapy crawl fefe -o web/fefelinks.json`
+- (optional) do processing
+- `cd web/ ; python -m SimpleHTTPServer`
+- point browser to localhost:8000
+
+## License
+http://opensource.org/licenses/MIT
---
+++ dietfefe/spiders/fefe.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from dietfefe.items import DietfefeItem
+
+class FefeSpider(scrapy.Spider):
+    name = "fefe"
+    allowed_domains = ["blog.fefe.de"]
+    start_urls = (
+        'https://blog.fefe.de/',
+    )
+
+    def parse(self, response):
+        for link in response.xpath('//ul/li/a/@href'):
+            item          = DietfefeItem()
+            item['link']  = link.extract()
+            if not "?ts" in item['link']:
+                yield item
